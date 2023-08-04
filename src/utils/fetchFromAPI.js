92a5b3836c4f03abc7aef2e959b87017649d01e0
Shah Akshar <72061051+Shahakshar@users.noreply.github.com>
2023-08-04 19:02:56 +0000

    done

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -4,7 +4,7 @@ export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
   headers: {
-    'X-RapidAPI-Key': 'b30efe13a7msh2a717fc0b37a661p1a735ajsn12406da9fc32',
+    'X-RapidAPI-Key': '??',
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
   }
 };
